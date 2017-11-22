a14f47d385d0b1b86958990c58d6a547c761933c
lightway <lightway.zhi@gmail.com>
2017-11-22 17:18:03 +0800

    test

--- bin/test.sh
+++ bin/test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-/usr/bin/expect <(cat <<'EOF'
-set timeout -1
-spawn ssh root@106.15.207.214
-expect "*password:"
-send "lightway\r"
-expect "*#"
-send "ll\r"
-expect "*#"
-interact
-expect eof
-EOF
-)
+DIR=`S=\`readlink "$0"\`; [ -z "$S" ] && S=$0; dirname $S`
+echo '$0 = '${0}
+echo 'readlink $0 ='`readlink $0`
+E='null';
+test -z `readlink $0` && E='not null'
+echo $E
