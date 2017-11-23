466653558282f7335959b042da8011bd4f0ab56c
lightway <lightway.zhi@gmail.com>
2017-11-23 18:03:25 +0800

    password way and key way for login

--- bin/lshell.sh
+++ bin/lshell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=`S=\`readlink "$0"\`; [ -z "$S" ] && S=$0; dirname $S`
 cd $DIR
-lw_except(){
+lw_expect_pwd(){
     /usr/bin/expect -c "
 set timeout 10
 spawn ssh $1
@@ -12,25 +12,37 @@ interact
 "
 }
 
+lw_expect_key(){
+    /usr/bin/expect -c "
+set timeout 10
+spawn ssh $1
+expect \"*passphrase*\"
+send \"$2\r\"
+expect \"*password:\"
+send \"$2\r\"
+expect \"*#\"
+interact
+"
+}
+
 lw_add(){
 echo $1
-    echo $1>>../data/test.conf
+    echo $1>>../data/data
     echo 'success'
 }
 
 lw_ssh(){
-    conf=`grep "^\$1\b" ../data/test.conf`
+    conf=`grep "^\$1\b" ../data/data`
     
     if test -n "$conf"
     then
     one=($conf)
-    case $one[4] in
-        key)
-            echo 'no work';;
-        password)
-            lw_except ${one[2]}"@"${one[1]} ${one[3]};;
-        *)
-            lw_except ${one[2]}"@"${one[1]} ${one[3]};;
+echo ${one[4]}
+    case ${one[4]} in
+        key)  
+            lw_expect_key ${one[1]} ${one[3]};;
+        pwd)
+            lw_expect_pwd ${one[2]}"@"${one[1]} ${one[3]};;
     esac
     else
         echo 'no found option !'
