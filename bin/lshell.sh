fc03654cc2585c9ca4777e88f798c5b4dee51077
lightway <lightway.zhi@gmail.com>
2018-01-23 21:49:37 +0800

    add origin ssh login type to response to expect login type by the iterm2 dead

--- bin/lshell.sh
+++ bin/lshell.sh
@@ -16,10 +16,10 @@ lw_expect_key(){
     /usr/bin/expect -c "
 set timeout 10
 spawn ssh $1
-expect \"*passphrase*\"
-send \"$2\r\"
-expect \"*password:\"
-send \"$2\r\"
+expect {
+\"*passphrase*\" {send \"$2\r\"}
+\"*password:\" {send \"$2\r\"}
+}
 expect \"*#\"
 interact
 "
@@ -31,7 +31,29 @@ echo $1
     echo 'success'
 }
 
+o_ssh(){
+echo 'this is original ssh login ...'
+    conf=`grep "^\$1\b" ../data/data`
+    
+    if test -n "$conf"
+    then
+    one=($conf)
+echo 'please press command + v to login'
+#copy password
+echo ${one[3]} | pbcopy
+    case ${one[4]} in
+        key)
+            ssh ${one[1]};;
+        pwd)
+            ssh ${one[2]}"@"${one[1]};;
+    esac
+    else
+        echo 'no found option !'
+    fi
+}
+
 lw_ssh(){
+echo 'this is expect auto login ...'
     conf=`grep "^\$1\b" ../data/data`
     
     if test -n "$conf"
@@ -52,13 +74,27 @@ echo ${one[4]}
 
 case $1 in
     help)
-        echo "help";;
+	if [ $2 ];
+	then
+	echo $2
+	else
+	echo "help"
+	fi;;
     -help)
         echo "help";;
     -add)
         lw_add "$2";;
     [a-z]*)
-        lw_ssh $1;;
-        
+	if [ $2 ];
+	then
+	  case $2 in
+	    -o)
+	      o_ssh $1;;
+	    -d)
+	      lw_ssh $1;;
+	  esac
+	else
+	lw_ssh $1
+	fi;;
 esac
 
